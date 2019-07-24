c938dfcfb5ea8894c7c7c0e3aead0dc1fa6fe822
Gunnar Hemmeryckx <gunnar.hemmeryckx@gmail.com>
2019-07-24 23:18:36 +0200

    swapped files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
-$('.nav-link, .navbar-brand').click(function() {
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+$(document).ready(function () {
+  $('.nav-link, .navbar-brand').click(function () {
     var sectionTo = $(this).attr('href');
     $('html, body').animate({
       scrollTop: ($(sectionTo).offset().top - 50)
     }, 1200);
+  });
+  
+  var delay = 250;
+  $(".progress-bar").each(function (i) {
+    $(this).delay(delay * i).animate({ width: $(this).attr('aria-valuenow') + '%' }, delay);
+  });
 });
